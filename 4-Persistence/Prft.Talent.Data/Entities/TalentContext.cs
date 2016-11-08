f519ded66735c606a3a1c334928a01f4f4e17aff
Rajesh K <r.kaliyugavarathan@perficient.com>
2016-11-08 14:36:00 +0530

    Added Test Project and regenerated poco classes

--- 4-Persistence/Prft.Talent.Data/Entities/TalentContext.cs
+++ 4-Persistence/Prft.Talent.Data/Entities/TalentContext.cs
@@ -5,7 +5,7 @@ namespace Prft.Talent.Data.Entities
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
 
-    public class TalentContext : DbContext
+    public partial class TalentContext : DbContext
     {
         public TalentContext()
             : base("name=TalentContext")
@@ -13,10 +13,15 @@ namespace Prft.Talent.Data.Entities
         }
 
         public virtual DbSet<employee> employees { get; set; }
+        public virtual DbSet<test> tests { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<employee>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<test>()
                 .Property(e => e.Name)
                 .IsUnicode(false);
         }
