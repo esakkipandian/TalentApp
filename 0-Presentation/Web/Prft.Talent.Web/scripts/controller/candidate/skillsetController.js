09ea2e1e766f308a7bb1ab24013b60aff53b7913
DESKTOP-TK3MHOK\Dev-User <praveen.nanjappan@perficient.com>
2016-11-19 19:12:19 +0530

    Skill set star rating and field validations

---
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/skillsetController.js
@@ -0,0 +1,46 @@
+﻿(function (angular) {
+    var SkillSetController = function ($scope, $controller) {
+
+        $scope.primaryskills = [
+            {
+                id: 'primaryskill',
+                dates: [
+                {
+
+                }
+                ]
+
+            }
+        ];
+
+        $scope.addNewPrimarySkill = function () {
+            var primarySkill = $scope.primaryskills.length + 1;
+            var date = new Date();
+            $scope.primaryskills.push({ 'id': 'primaryskill' + primarySkill, 'dates': { date } });
+            
+        };
+
+        $scope.removePrimarySkill = function (index) {
+            $scope.primaryskills.splice(index, 1);
+        };
+
+
+        $scope.open = function ($event, dt) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            dt.opened = true;
+        };
+
+        $scope.rate = 1;
+        $scope.max = 5;
+
+        $scope.ratingStates = { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' };
+
+        $scope.submit = function () {
+            $scope.submitted = true;
+        }
+
+    };
+    SkillSetController.$inject = ['$scope', '$controller'];
+    mainApp.controller('skillSetController', SkillSetController);
+})(angular);
