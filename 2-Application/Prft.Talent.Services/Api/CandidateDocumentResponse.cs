9db2a11703da791423b051b38cf58ff1f79ccc7f
Esakki Maharajan <esakki.maharajan@perficient.com>
2016-11-21 16:18:23 +0530

    added missing files

---
+++ 2-Application/Prft.Talent.Services/Api/CandidateDocumentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Prft.Talent.Domain.Talent;
+using Newtonsoft.Json;
+
+namespace Prft.Talent.Services.Api
+{
+    public class CandidateDocumentResponse : ResponseBase
+    {
+        [JsonProperty("entity")]
+        public IEnumerable<CandiateDocument> Entity { get; set; }
+    }
+}
