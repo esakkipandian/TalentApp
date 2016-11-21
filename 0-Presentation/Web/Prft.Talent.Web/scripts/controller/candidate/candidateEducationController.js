4a0a341d241a21db6b5c62a56f9f008fddedfedf
Dhilipkumar Selvaraj <dhilipkumar.selvaraj@perficient.com>
2016-11-21 09:16:21 +0530

    Educational Controller Edit inculding dropdown bind

--- 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/candidateEducationController.js
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/candidateEducationController.js
@@ -2,7 +2,29 @@
     var CandidateEducationController = function ($scope, $controller, DTColumnBuilder, commonAPIservice) {
         var _this = this;
         _this.service = commonAPIservice;
+       
 
+        loadColleges();
+        //Load Drop Down Values 
+        var loadColleges = function () {
+            var url = 'http://localhost:8078/api/colleges';
+            _this.service.loadRecords(url)
+                         .then(function (response) {
+                             $scope.Colleges = response;
+                         });
+        };
+
+        // Load Model data if Exsist
+        var loadEducationInformation = function () {
+            var url = 'http://localhost:8080/api/candidates/' + perfDatatable.recordId;
+            _this.service.loadRecords(url)
+                         .then(function (response) {
+                             $scope.EducationalInformation = response.data;
+                             perfDatatable.recordId = 0;
+                         });
+        }
+
+        // Save and update Records to the WebAPI
         $scope.save = function () {
             _this.service.add('http://localhost:8080/api/Candidates/AddPersonalInformation/', $scope.EducationalInformation);
         };
