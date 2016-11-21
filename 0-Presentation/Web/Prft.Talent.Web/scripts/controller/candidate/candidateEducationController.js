230f75fcd1af3732ea04ac62b9668096b9efd62d
Rajesh K <r.kaliyugavarathan@perficient.com>
2016-11-21 12:37:59 +0530

    added dropdown for colleges.

--- 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/candidateEducationController.js
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/candidateEducationController.js
@@ -2,17 +2,20 @@
     var CandidateEducationController = function ($scope, $controller, DTColumnBuilder, commonAPIservice) {
         var _this = this;
         _this.service = commonAPIservice;
-       
 
-       // loadColleges();
-        //Load Drop Down Values 
-       // var loadColleges = function () {
+        var loadColleges = function () {
             var url = 'http://localhost:8080/api/colleges';
             _this.service.loadRecords(url)
                          .then(function (response) {
                              $scope.Colleges = response.data.collges;
                          });
-       // };
+        };
+
+
+        $scope.save = function () {
+            _this.service.add('http://localhost:8080/api/Candidates/AddEducationInformation/', $scope.EducationalInformation);
+        };
+
 
         // Load Model data if Exsist
         var loadEducationInformation = function () {
@@ -24,10 +27,8 @@
                          });
         }
 
-        // Save and update Records to the WebAPI
-        $scope.save = function () {
-            _this.service.add('http://localhost:8080/api/Candidates/AddPersonalInformation/', $scope.EducationalInformation);
-        };
+        //Load Drop Down Values 
+         loadColleges();
     };
     CandidateEducationController.$inject = ['$scope', '$controller', 'DTColumnBuilder', 'commonAPIservice'];
     mainApp.controller('candidateEducationController', CandidateEducationController);
