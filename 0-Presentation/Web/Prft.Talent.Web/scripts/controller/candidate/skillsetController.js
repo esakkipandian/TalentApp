9e2f65aadcde2a29813305bb79c14ec3914623a8
DESKTOP-TK3MHOK\Dev-User <praveen.nanjappan@perficient.com>
2016-11-27 23:10:16 +0530

    Skillset datepicker and validation changes

--- 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/skillsetController.js
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/skillsetController.js
@@ -1,20 +1,20 @@
 ﻿(function (angular) {
-    var SkillSetController = function ($scope, $controller, commonAPIservice, candidateCommonServices) {
+    var SkillSetController = function ($scope, $controller, $filter, commonAPIservice, candidateCommonServices) {
+
+        //Initialization
         var _this = this;
         _this.title = "Skillset";
         _this.service = commonAPIservice;
         _this.CandidateCommonServices = candidateCommonServices;
-        $scope.skillset = [
-            {
-                id: 0,
-                dates: [{}]
-            }
-        ];
-
+        $scope.skillset = [];
+        $scope.dates = []; 
+        
+        //Button Tooltips
         $(document).ready(function () {
             $('[data-toggle="tooltip"]').tooltip();
         });
 
+        //Function to load Skills Autocomplete
         var loadSkillSets = function () {
             var url = 'http://localhost:8080/api/skillset';
             _this.service.loadRecords(url)
@@ -23,35 +23,7 @@
                          });
         };
 
-        $scope.addNewSkill = function () {
-            var primarySkill = $scope.skillset.length + 1;
-            var date = new Date();
-            $scope.skillset.push({ 'id': primarySkill, 'dates': { date } });
-        };
-
-        $scope.removeSkill = function (index) {
-            $scope.skillset.splice(index, 1);
-        };
-
-        $scope.saveSkill = function (recordIndex) {
-            var candidateId = _this.CandidateCommonServices.getCandidateId();
-            if (candidateId > 0) {
-                var skillset = $scope.skillset[recordIndex];
-                skillset.candidateId = candidateId;
-                _this.service.add('http://localhost:8080/api/CandidateSkillSet/AddCandidateSkillSet/', skillset)
-                    .then(function (response) {
-                        perfUtils.getInstance().successMsg(_this.title + ' added Successfully!');
-                    });
-            }
-        };
-
-        $scope.opened = [];
-        $scope.openDatePicker    = function ($event, index) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            $scope.opened[index] = true;
-        };
-
+        //Function to load Candidate Skill Sets 
         var loadCandidateSkillSets = function () {
             var candidateId = _this.CandidateCommonServices.getCandidateId();
             if (candidateId > 0) {
@@ -62,22 +34,91 @@
                                  if (skillSetLength > 0) {
                                      $scope.skillset = response.data;
                                      $scope.dates = [{}];
+                                     angular.forEach($scope.skillset, function (value, key) {                                         
+                                             var sinceLastUsed = new Date($scope.skillset[key].sinceLastUsed);
+                                             $scope.skillset[key].sinceLastUsed = ((sinceLastUsed.getMonth() + 1) + "/" + sinceLastUsed.getDate() + "/" + sinceLastUsed.getFullYear());
+                                             
+                                     });
                                  }
                                  else {
-                                     $scope.skillset = [
-                                         {
-                                             id: 0,
-                                             dates: [{}]
-                                         }
-                                     ];
+                                     $scope.skillset = [$scope.candidateSkillSetForm];
                                      $scope.dates = [{}];
                                  }
                              });
             }
         };
 
-        $scope.register = {};
+        //Function to save and add new Skill
+        $scope.addNewSkill = function (recordIndex) {            
+            var skillset = $scope.skillset[recordIndex];
+            if (skillset.pk >= 0 )
+                $scope.skillset.push({});
+            else {
+                if (!skillset.skillId || !skillset.rating || !skillset.sinceLastUsed || typeof skillset.isPrimary == 'undefined') {
+                    perfUtils.getInstance().successMsg('All Fields are mandatory');
+                    return;
+                }
+                var candidateId = _this.CandidateCommonServices.getCandidateId();
+                    if (candidateId > 0) {
+                        var skillset = $scope.skillset[recordIndex];
+                            skillset.candidateId = candidateId;
+                            _this.service.add('http://localhost:8080/api/CandidateSkillSet/AddCandidateSkillSet/', skillset)
+                                .then(function (response) {                                   
+                                    perfUtils.getInstance().successMsg(_this.title + ' added Successfully!');
+                                });     
+                    }
+                    $scope.skillset.push({});
+            }
+        };
 
+        //Function to Save skill
+        $scope.saveSkill = function (recordIndex) {
+            var skillset = $scope.skillset[recordIndex];
+            if (!skillset.skillId || !skillset.rating || !skillset.sinceLastUsed || typeof skillset.isPrimary == 'undefined') {
+                perfUtils.getInstance().successMsg('All Fields are mandatory');
+                return;
+            }
+            var candidateId = _this.CandidateCommonServices.getCandidateId();
+            if (candidateId > 0) {
+                if (skillset.pk > 0) {
+                    alert("Updated Successfully");                 
+                }
+                else
+                {
+                    skillset.candidateId = candidateId;
+                    _this.service.add('http://localhost:8080/api/CandidateSkillSet/AddCandidateSkillSet/', skillset)
+                        .then(function (response) {                            
+                            loadCandidateSkillSets();
+                            perfUtils.getInstance().successMsg(_this.title + ' added Successfully!');
+                        });
+                }
+            }
+        };
+
+        //Function to Delete Skill
+        $scope.deleteSkill = function (recordIndex) {
+            var candidateId = _this.CandidateCommonServices.getCandidateId();
+            var skillset = $scope.skillset[recordIndex]; 
+            if (candidateId > 0 && typeof skillset.isPrimary != 'undefined') {
+                _this.service.updateDel('http://localhost:8080/api/CandidateSkillSet/DeleteCandidateSkillSet/',skillset)
+                    .then(function (response) {
+                        $scope.skillset.splice(recordIndex, 1);
+                        perfUtils.getInstance().successMsg(_this.title + ' deleted Successfully!');
+                    });
+            }
+            else
+                $scope.skillset.splice(recordIndex, 1);
+        };
+       
+        //Function to open Datepicker
+        $scope.open   = function ($event, dt) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            dt.opened = true;
+        };
+
+        //Load Skill Type Dropdown
+        $scope.register = {};
         $scope.register.skillsets = [{
             value: true,
             name: "Primary"
@@ -85,12 +126,14 @@
             value: false,
             name: "Secondary"
         }];
-
+               
+        //Star Rating Directive
         $scope.ratingStates = { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' };
+
+        //Functions during page load
         loadCandidateSkillSets();
         loadSkillSets();
-
     };
-    SkillSetController.$inject = ['$scope', '$controller', 'commonAPIservice', 'candidateCommonServices'];
+    SkillSetController.$inject = ['$scope', '$controller','$filter', 'commonAPIservice', 'candidateCommonServices'];
     mainApp.controller('skillSetController', SkillSetController);
 })(angular);
