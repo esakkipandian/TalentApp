f05bef8512901ca4b0a765bbec00cd834ca53487
Sujitha <sujitha.prabhakaran@perficient.com>
2016-11-28 18:47:45 +0530

    Validation Checkin

--- 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/candidateEducationController.js
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/candidate/candidateEducationController.js
@@ -9,6 +9,7 @@
           {
               id: 'q',
               specialization: '',
+              passedyear: [{}],
               percentage: '',
           }
         ];
@@ -72,14 +73,18 @@
 
         $scope.addNewQualification = function () {
             var qualification = $scope.qualification.length + 1;
-            $scope.qualification.push({ 'id': 'new', 'specialization': '', 'percentage': '' });
-
+            $scope.qualification.push({ 'id': 'q' + qualification, 'specialization': '', 'passedyear': [{}], 'percentage': '' });
         };
 
         $scope.removeQualification = function (index) {
             $scope.qualification.splice(index, 1);
         };
 
+        $scope.open = function ($event, dt) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            dt.opened = true;
+        };
 
         // Load Model data if Exsist
         var loadEducationInformation = function () {
