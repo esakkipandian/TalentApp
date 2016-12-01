340253f97c0b46deca78f52bbdd9f73f8c6816e6
Praveen Kumar Nanjappan <praveen.nanjappan@perficient.com>
2016-12-01 13:09:02 +0530

    Scheduled Candidates Grid

--- 0-Presentation/Web/Prft.Talent.Web/scripts/controller/interviewFeedbackController.js
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/interviewFeedbackController.js
@@ -1,5 +1,23 @@
 ﻿(function (angular) {  
-    var InterviewFeedbackController = function ($scope, $controller) {
+    var InterviewFeedbackController = function ($scope, $controller, DTColumnBuilder) {
+        $scope.dateofinterview = new Date();
+        var _this = this;
+        $scope.dtColumns = [
+            DTColumnBuilder.newColumn('fullName').withTitle('Candidate Name'),
+            DTColumnBuilder.newColumn('scheduledate').withTitle('Schedule Date'),
+            DTColumnBuilder.newColumn('scheduletime').withTitle('Schedule Time')
+        ];
+        var vm = {
+            'title': 'Scheduled Candidates',
+            'formId': 'interviewForm',
+            'scope': $scope,
+            'addUrl': perfUrl['addDesignation'],
+            'updateUrl': perfUrl['updateDesignation'],
+            'deleteUrl': perfUrl['deleteDesignation'],
+            'loadListUrl': perfUrl['loadCandidateInformation'],
+            'actions': false
+        };
+        angular.extend(this, $controller('AbstractController', { _this: _this, vm: vm }));
 
         $scope.doi = {
             opened: false
@@ -7,9 +25,9 @@
 
         $scope.doiDate = function () {
             $scope.doi.opened = true;
-        };
-     
+        };    
+      
     };
-    InterviewFeedbackController.$inject = ['$scope', '$controller'];
+    InterviewFeedbackController.$inject = ['$scope', '$controller', 'DTColumnBuilder'];
     mainApp.controller('interviewFeedbackController', InterviewFeedbackController);
 })(angular);
