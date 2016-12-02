f8ee09716fa05f01d33c7ce2e8ad934fc08d2534
DESKTOP-TK3MHOK\Dev-User <sunil.sukumaran@perficient.com>
2016-12-02 14:49:14 +0530

    Dynamic dropdown

--- 0-Presentation/Web/Prft.Talent.Web/scripts/controller/interviewFeedbackController.js
+++ 0-Presentation/Web/Prft.Talent.Web/scripts/controller/interviewFeedbackController.js
@@ -29,7 +29,31 @@
 
         $scope.doiDate = function () {
             $scope.doi.opened = true;
-        };    
+        };
+
+        var ratingLevel = {
+            "availableOptions": [
+             { id: '0', name: 'NE' },
+            { id: '1', name: '1' },
+            { id: '2', name: '2' },
+            { id: '3', name: '3' },
+            { id: '4', name: '4' },
+            { id: '5', name: '5' }
+            ],
+            educationBackgroundRating: { id: '0', name: 'NE' },
+            technicalKnowledgeRating: { id: '0', name: 'NE' },
+            experienceLevelRating: { id: '0', name: 'NE' },
+            applicationSkillRating: { id: '0', name: 'NE' },
+            abilityDemonstrateRating: { id: '0', name: 'NE' },
+            communicationSkillsRating: { id: '0', name: 'NE' },
+            analyticalSkillsRating: { id: '0', name: 'NE' },
+            leadershipQualitiesRating: { id: '0', name: 'NE' },
+            interestLevelRating: { id: '0', name: 'NE' }
+
+        };
+        $scope.Feedback = {};
+        $scope.Feedback.RatingOptions = ratingLevel;     
+    };
       
     };
     InterviewFeedbackController.$inject = ['$scope', '$controller', 'DTColumnBuilder'];
